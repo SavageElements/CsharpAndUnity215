6f16720fb716d5ec7be8b84461971dfc173e2c8c
J Rockwell Seebach <jrs@ironpinephoto.com>
2022-07-06 14:32:05 -0700

    Add project files.

---
+++ CsharpAndUnity215/Maths.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpAndUnity215
+{
+    public class  MathMethods
+    {
+        public static double DoubleUP (double X)
+        {
+            double M1 = X;
+            double dubUP = M1 * 2;
+            return dubUP;
+        }
+
+        public static double PITime(double X)
+        {
+            double M2 = X;
+            double piTime = M2 * 3.14;
+            return piTime;
+        }
+
+        public static double AddSeven(double X)
+        {
+            double M3 = X;
+            double plusSEV = M3 + 7;
+            return plusSEV;
+        }
+    }
+}
---
+++ CsharpAndUnity215/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CsharpAndUnity215
+{
+    class Program
+    {
+        public class DATAInput 
+        {
+            public double dataInput { get; set; }
+
+        }
+        public static void Main(string[] args)
+        {
+            MathMethods newMethod = new MathMethods();
+            
+            Console.WriteLine("Step right up! Thats Right Folks! \n" +
+            "Pick a number, any whole number.");
+
+            double dataInput = Convert.ToDouble(Console.ReadLine());
+            double result1 = MathMethods.DoubleUP(dataInput);
+            double result2 = MathMethods.PITime(dataInput);
+            double result3 = MathMethods.AddSeven(dataInput);
+
+            Console.WriteLine("Your number doubled is " + result1);
+            Console.WriteLine("Your number times PI is " + result2);
+            Console.WriteLine("Your number plus Seven is " + result3);
+
+        }
+    }
+}
